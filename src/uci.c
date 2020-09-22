450cb225876d6262587cde59500549ff260f162d
Benjamin Paul <bpaul848@gmail.com>
2020-09-22 17:42:45 +1000

    Add increment to allocated time

--- src/uci.c
+++ src/uci.c
@@ -415,7 +415,8 @@ void *go(void *args) {
     info.maxDepth = maxDepth;
     info.startTime = getTime();
     int time = (board.turn == WHITE ? wtime : btime);
-    info.time = (movetime != -1 ? movetime : initTimeManagement(&board, time, MAX(movestogo, 2)));
+    int inc = (board.turn == WHITE ? winc : binc);
+    info.time = (movetime != -1 ? movetime : initTimeManagement(&board, time, inc, MAX(movestogo, 2)));
 
     initThreadSearch(threads, board, info);
     
